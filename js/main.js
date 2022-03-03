20cd7345728fab32fad911bb24fe372ad7fa5e4d
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-03 15:39:30 -0600

    Added personal code, get element ID for search bar

--- js/main.js
+++ js/main.js
@@ -30,6 +30,12 @@ function updateCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+//---   Personal COde
+var searchName = document.getElementById("coffeeName"); // Grab search bar
+
+
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
